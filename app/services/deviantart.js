774329864343a2b866656e19548539cca8a29e5a
Don Henton <donhenton@gmail.com>
2016-06-17 07:02:34 -0500

    working call to da 2

--- app/services/deviantart.js
+++ app/services/deviantart.js
@@ -1,70 +1,48 @@
 var rp = require("request-promise");
-var request = require('request');
-module.exports = function (config) {
+
+module.exports = function (conf) {
 
 
     var daoService = {};
     //server stuff here
-    
+
     daoService.getCredentials = function ()
     {
-        var uri =   
-        'https://www.deviantart.com/oauth2/token;
+        var uri = 'https://www.deviantart.com/oauth2/token';
+
         var options = {
             method: 'POST',
-            uri:  uri,
-            json:true,
-            gzip:true,
+            uri: uri,
+            json: true,
+            gzip: true,
             headers:
                     {
                         'User-Agent': 'Request-Promise'
+                    },
+            form:
+                    {
+                        client_secret: conf.client_secret,
+                        client_id: conf.client_id,
+                        grant_type: 'client_credentials'
                     }
-            
+
         };
-        
-         
+
+
 
         rp(options)
                 .then(function (body) {
-                   console.log("success in  service "+JSON.stringify(body)) 
+                    console.log("success in  service " + JSON.stringify(body))
                 })
                 .catch(function (err) {
-                    
-                   console.log("error in  service\n\n\n"+err.message) 
+
+                    console.log("error in  service\n\n\n" + err.message)
                 });
 
 
     }
-    
-    daoService.workinggetCredentials = function ()
-    {
-        var uri =   
-        'https://www.deviantart.com/oauth2/token?client_secret=<secret>&client_id=<id>&grant_type=client_credentials';
-        var options = {
-            method: 'GET',
-            uri:  uri,
-            json:true,
-            gzip:true,
-            headers:
-                    {
-                        'User-Agent': 'Request-Promise'
-                    }
-            
-        };
-        
-         
 
-        rp(options)
-                .then(function (body) {
-                   console.log("success in  service "+JSON.stringify(body)) 
-                })
-                .catch(function (err) {
-                    
-                   console.log("error in  service\n\n\n"+err.message) 
-                });
-
-
-    }
+   
     return daoService;
 
 }
