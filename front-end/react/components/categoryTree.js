ccb4445cca5ca45968109200688a5763dae8a76e
Don Henton <donhenton@gmail.com>
2016-06-28 20:14:06 -0500

    working tree

--- front-end/react/components/categoryTree.js
+++ front-end/react/components/categoryTree.js
@@ -6,49 +6,9 @@ import deviantService from './../services/deviantService';
 import postal from 'postal';
 
 
-function generateTreeNodes(treeNode) {
-  const arr = [];
-  const key = treeNode.props.eventKey;
-  for (let i = 0; i < 3; i++) {
-    arr.push({name: `leaf ${key}-${i}`, key: `${key}-${i}`});
-  }
-  return arr;
-}
+ 
 
-function setLeaf(treeData, curKey, level) {
-  const loopLeaf = (data, lev) => {
-    const l = lev - 1;
-    data.forEach((item) => {
-      if ((item.key.length > curKey.length) ? item.key.indexOf(curKey) !== 0 :
-        curKey.indexOf(item.key) !== 0) {
-        return;
-      }
-      if (item.children) {
-        loopLeaf(item.children, l);
-      } else if (l < 1) {
-        item.isLeaf = true;
-      }
-    });
-  };
-  loopLeaf(treeData, level + 1);
-}
-
-function getNewTreeData(treeData, curKey, child, level) {
-  const loop = (data) => {
-    if (level < 1 || curKey.length - 3 > level * 2) return;
-    data.forEach((item) => {
-      if (curKey.indexOf(item.key) === 0) {
-        if (item.children) {
-          loop(item.children);
-        } else {
-          item.children = child;
-        }
-      }
-    });
-  };
-  loop(treeData);
-  setLeaf(treeData, curKey, level);
-}
+ 
 
 const categoryTree = React.createClass({
   propTypes: {},
@@ -62,42 +22,29 @@ const categoryTree = React.createClass({
       
       var me = this;
       
-//       postal.subscribe({
-//            channel: "deviant-system",
-//            topic: "categoryData",
-//            callback: function (data, envelope) {
-//                
-//                var leafMarked = data.categories.map((cat) => 
-//                        {
-//                            
-//                     var item = {};
-//                     Object.assign(item,cat)
-//             
-//                     item.isLeaf = !cat.has_subcategory;
-//                     return item;
-//                            
-//                })
-//                
-//                
-//                
-//                me.setState({treeData: leafMarked})
-//            }
-//        });  
-      
-      
-      
-    setTimeout(() => {
-      this.setState({
-        treeData: [
-          {name: 'pNode 01', key: '0-0',children:[{'name':'fred','key':'0-0-4'}]},
-          {name: 'pNode 02', key: '0-1'},
-          {name: 'pNode 03', key: '0-2', isLeaf: true},
-        ],
-        checkedKeys: ['0-0'],
-      });
-    }, 100);
+        postal.subscribe({
+             channel: "deviant-system",
+             topic: "categoryData",
+             callback: function (data, envelope) {
+                 
+                 var leafMarked = data.categories.treeData.map((cat) => 
+                         {
+                             
+                      var item = {};
+                      Object.assign(item,cat)
+              
+                      item.isLeaf = (item.children.length == 0);
+                      return item;
+                             
+                 })
 
-    deviantService.getCategories(null);
+                 me.setState({treeData: leafMarked})
+             }
+         });  
+      
+      
+   
+    deviantService.getCategories(null,true);
 
 
 
@@ -112,16 +59,31 @@ const categoryTree = React.createClass({
     });
   },
   onLoadData(treeNode) {
+      console.log("current Request "+treeNode.props.eventKey);
+      var me = this;
+      var retVal = deviantService.getCategories(treeNode.props.eventKey,false);
       
-      console.log(treeNode.props.key)
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const treeData = [...this.state.treeData];
-        getNewTreeData(treeData, treeNode.props.eventKey, generateTreeNodes(treeNode), 2);
-        this.setState({treeData});
-        resolve();
-      }, 500);
-    });
+      retVal.then(function(data)
+      {
+          me.setState({treeData: data.treeData});
+      });
+      
+      
+      return retVal;
+      
+      
+      
+      
+      
+//       
+//    return new Promise((resolve) => {
+//      setTimeout(() => {
+//        const treeData = [...this.state.treeData];
+//        getNewTreeData(treeData, treeNode.props.eventKey, generateTreeNodes(treeNode), 2);
+//        this.setState({treeData});
+//        resolve();
+//      }, 500);
+//    });
   },
   render() {
     const loop = (data) => {
@@ -129,7 +91,7 @@ const categoryTree = React.createClass({
         if (item.children) {
           return <TreeNode title={item.name} key={item.key}>{loop(item.children)}</TreeNode>;
         }
-        return <TreeNode title={item.name} key={item.key} isLeaf={item.isLeaf} disabled={item.key === '0-0-0' ? true : false} />;
+        return <TreeNode title={item.name} key={item.key} isLeaf={item.isLeaf}  />;
       });
     };
     const treeNodes = loop(this.state.treeData);
--- front-end/react/services/deviantService.js
+++ front-end/react/services/deviantService.js
@@ -1,5 +1,6 @@
 import ProxyService from './proxyService';
 import postal from 'postal';
+import treeService from './treeService';
 
 class DeviantService
     {
@@ -12,37 +13,56 @@ class DeviantService
             
             
             console.log("base "+baseURL)
-        
+        this.baseData = null;
              
     }
 
     
     /**
      * labels include slashes
+     * resolvePromise 
      */
-    getCategories(categoryLabel)
+    getCategories(categoryLabel,resolvePromise)
     {
+        var me = this;
+        
+        if (!resolvePromise)
+        {
+            return this.proxyService.getCategories(categoryLabel)
+                    .then(function(data)
+            {
+                var dData = JSON.parse(data)
+                var processedData =  treeService.addKeys(dData,me.baseData);
+                 me.baseData = processedData;
+                return processedData;
+            });
+        }
+        
+        
         this.proxyService.getCategories(categoryLabel)
         .then(function(dataAsString)
         {
             var data = JSON.parse(dataAsString)
              
-            var simpleData = data.categories.map((cat)=>{
-                
-                var s = {};
-                s.name = cat.title;
-                s.key = cat.catpath;
-                s.catpath = cat.catpath;
-                s.has_subcategory = cat.has_subcategory;
-                 return s;
-                
-            })
+//            var simpleData = data.categories.map((cat)=>{
+//                
+//                var s = {};
+//                s.name = cat.title;
+//                s.key = cat.catpath;
+//                s.catpath = cat.catpath;
+//                s.has_subcategory = cat.has_subcategory;
+//                 return s;
+//                
+//            })
             
+            var processedData = treeService.addKeys(data,me.baseData);
+            
+            me.baseData = processedData;
             
             postal.publish({
                 channel: "deviant-system",
                 topic: "categoryData" ,
-                data: {label: categoryLabel, categories: simpleData} 
+                data: {label: categoryLabel, categories: processedData} 
             });
         }) 
         .catch(function(err) {
@@ -50,11 +70,7 @@ class DeviantService
             //"400 - {"message":"key: name Restaurant Name cannot be blank,key: zipCode Zipcode cannot be blank,key: state State cannot be blank,key: city City cannot be blank","errorClass":"com.dhenton9000.restaurant.service.impl.ValidatorFailureException"}"
 
 
-            postal.publish({
-                channel: "restaurants-system",
-                topic: "item.save.request.complete" ,
-                data: {error: err.message}
-            });
+            console.log("error "+err.message)
         })
           
     }
